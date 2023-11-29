c31fc7bcb2fc8a351748587a08ebef34934c5337
skif-cezar <skif.cezar@gmail.com>
2023-11-29 18:42:07 +0300

    feat: add card application

---
+++ src/app/components/card/Card.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "src/app/components/card/Card.module.scss";
+
+/**
+ * Card props
+ */
+export interface CardProps {
+  employee: string;
+  date: string;
+  name: string;
+  parlor: string;
+  status: string;
+}
+
+/**
+ * Card application component
+ */
+export const Card: React.FC<CardProps> = (props: CardProps) => {
+  const CARD_STYLES = clsx(styles.card);
+  const EMPLOYEE_STYLES = clsx(styles.employee);
+  const DATE_STYLES = clsx(styles.date);
+  const NAME_STYLES = clsx(styles.name);
+  const PARLOR_STYLES = clsx(styles.parlor);
+  const STATUS_STYLES = clsx(styles.status);
+  const STATUS_OPEN_STYLES = clsx(styles.status_open);
+
+  return (
+    <div className={CARD_STYLES}>
+      <p className={EMPLOYEE_STYLES}>{props.employee}</p>
+      <p className={DATE_STYLES}>{props.date}</p>
+      <p className={NAME_STYLES}>{props.name}</p>
+      <p className={PARLOR_STYLES}>{props.parlor}</p>
+      <p className={STATUS_STYLES}>
+        <span className={STATUS_OPEN_STYLES}>{props.status}</span>
+      </p>
+    </div>
+  );
+};
--- src/app/logic/pages/user/ApplicationUser.tsx
+++ src/app/logic/pages/user/ApplicationUser.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import clsx from "clsx";
+import styles from "src/app/logic/pages/user/UserPage.module.scss";
+import {Card} from "src/app/components/card/Card";
 
 /**
  *  Path to application user
@@ -9,8 +12,61 @@ export const APPLICATION_USER_URL = "/user/applications";
  * Application user
  */
 export const ApplicationUser: React.FC = () => {
+  const TABLE_STYLES = clsx(styles.table);
+  const HEADER_STYLES = clsx(styles.table__heder);
+  const TITLE_EMPLOYEE_STYLES = clsx(styles.title_employee);
+  const TITLE_DATE_STYLES = clsx(styles.title_date);
+  const TITLE_NAME_STYLES = clsx(styles.title_name);
+  const TITLE_PARLOR_STYLES = clsx(styles.title_parlor);
+  const TITLE_STATUS_STYLES = clsx(styles.title_status);
+  const CONTAINER_STYLES = clsx(styles.container);
 
   return (
-    <p>Заявки пользователя</p>
+    <div className={TABLE_STYLES}>
+      <div className={HEADER_STYLES}>
+        <p className={TITLE_EMPLOYEE_STYLES}>Сотрудник</p>
+        <p className={TITLE_DATE_STYLES}>Дата</p>
+        <p className={TITLE_NAME_STYLES}>Тема заявки</p>
+        <p className={TITLE_PARLOR_STYLES}>Кабинет</p>
+        <p className={TITLE_STATUS_STYLES}>Статус</p>
+      </div>
+      <div className={CONTAINER_STYLES}>
+        <Card
+          employee="skif.cezar-8282hhbs2@gmil.com"
+          date="13.09.2015 14:45"
+          name="Не работает принтер и ксерокс"
+          parlor="Кабинет 456"
+          status="Открыта"
+        />
+        <Card
+          employee="skif.cezar-8282hhbs2@gmil.com"
+          date="13.09.2015 14:45"
+          name="Не работает принтер и ксерокс"
+          parlor="Кабинет 456"
+          status="Открыта"
+        />
+        <Card
+          employee="skif.cezar-8282hhbs2@gmil.com"
+          date="13.09.2015 14:45"
+          name="Не работает принтер и ксерокс"
+          parlor="Кабинет 456"
+          status="Открыта"
+        />
+        <Card
+          employee="skif.cezar-8282hhbs2@gmil.com"
+          date="13.09.2015 14:45"
+          name="Не работает принтер и ксерокс"
+          parlor="Кабинет 456"
+          status="Открыта"
+        />
+        <Card
+          employee="skif.cezar-8282hhbs2@gmil.com"
+          date="13.09.2015 14:45"
+          name="Не работает принтер и ксерокс"
+          parlor="Кабинет 456"
+          status="Открыта"
+        />
+      </div>
+    </div>
   );
 };
