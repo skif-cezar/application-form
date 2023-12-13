c5e34621166ab16a8b0bb784c57a97a0b1d4b876
skif-cezar <skif.cezar@gmail.com>
2023-12-13 19:35:30 +0300

    fix: get Apps data/show alert

--- src/app/logic/pages/admin/AdminPage.tsx
+++ src/app/logic/pages/admin/AdminPage.tsx
@@ -1,22 +1,16 @@
-import {memo, useState, useCallback, useEffect} from "react";
+import {memo, useState, useCallback} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {collection, query, orderBy, limit, getDocs, where} from "firebase/firestore";
+import {collection, query, orderBy, limit, getDocs} from "firebase/firestore";
 import {db} from "src/firebase";
-import {
-  ApplicationState,
-  addAppLastVisible,
-  addApplication,
-  clearApplication,
-} from "src/app/store/applications/slices/applicationSlice";
+import {clearApplication} from "src/app/store/applications/slices/applicationSlice";
 import {NavLink, Outlet, useNavigate} from "react-router-dom";
 import {useAuth} from "src/app/hooks/useAuth";
 import {MAIN_PAGE_PATH} from "src/app/logic/layout/Layout";
-import {UserState, addEmploye, addUserLastVisible, clearEmployees, removeUser} from "src/app/store/user/slices/userSlice";
+import {addEmploye, addUserLastVisible, clearEmployees, removeUser} from "src/app/store/user/slices/userSlice";
 import {Spinner} from "src/app/components/spinner/Spinner";
 import clsx from "clsx";
 import styles from "src/app/logic/pages/user/UserPage.module.scss";
 import {REGISTRATION_PAGE_PATH} from "src/app/components/registration/Registration";
-import {getFormatDate} from "src/app/utility/getFormatDate";
 import {APPLICATIONS_URL} from "src/app/logic/pages/admin/ApplicationsAll";
 import {AppState} from "src/app/store";
 import {EMPLOYEES_PAGE_URL} from "src/app/logic/pages/employees/EmployeesPage";
@@ -46,62 +40,6 @@ export const AdminPage = memo((): any => {
   // Получение данных о user из store
   const user = useSelector((state: AppState) => state.users.user);
   const userFullName = `${user!.lastName} ${user!.firstName} ${user!.surname}`;
-
-  const getApplicationData = useCallback(async (): Promise<void> => {
-    setLoading(true);
-
-    // Получение заявок из Firestore по условию с лимитом по 8 записей
-    const appData = query(collection(db, "applications"),
-      orderBy("date", "desc"),
-      limit(8));
-    const querySnapshot = await getDocs(appData);
-
-    if(querySnapshot.docs.length !== 0) {
-      // Получение последних видимых записей
-      const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
-
-      // Добавление последних видимых данных заявки в store
-      dispatch(
-        addAppLastVisible(lastVisible),
-      );
-
-      querySnapshot.forEach(async(doc: any) => {
-        // id заявки
-        const {id}: ApplicationState = doc;
-        const {idUser, title, description, parlor, comment, status}: ApplicationState = doc.data();
-        // Перевод даты из Firestore в строку
-        const date = getFormatDate(doc.data().date.seconds);
-
-        // Получение данных user из Firestore по условию
-        const userData = query(collection(db, "users"),
-          where("idUser", "==", idUser));
-
-        const querySnapshotUser = await getDocs(userData);
-
-        querySnapshotUser.forEach((userDoc: any) => {
-          const {firstName, lastName, surname}: UserState = userDoc.data();
-          const author = `${lastName} ${firstName} ${surname}`;
-
-          // Добавление данных заявки в store
-          dispatch(
-            addApplication({
-              id,
-              idUser,
-              author,
-              title,
-              description,
-              parlor,
-              date,
-              comment,
-              status,
-            }),
-          );
-        });
-      });
-    }
-    setLoading(false);
-
-  }, []);
 
   const getEmployees = useCallback(async (): Promise<void> => {
     setLoading(true);
@@ -136,10 +74,6 @@ export const AdminPage = memo((): any => {
 
   }, []);
 
-  useEffect(() => {
-    getApplicationData();
-  }, []);
-
   if (isAuth) {
     return (
       <>
@@ -150,7 +84,6 @@ export const AdminPage = memo((): any => {
                 to={APPLICATIONS_URL}
                 className={({isActive}: { isActive: boolean }) => isActive ? LINK_ACTIVE__STYLES : LINK_STYLES}
                 onClick={() => {
-                  getApplicationData();
                   dispatch(clearEmployees());
                 }}
               >
